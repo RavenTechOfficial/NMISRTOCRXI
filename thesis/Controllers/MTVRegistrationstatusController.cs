dc8e4c830d3dab56677561d60ccd043484dc3053
Jessc <Jessc@DESKTOP-G6KJNEA>
2023-10-16 23:52:33 +0800

    status

--- thesis/Controllers/MTVRegistrationstatusController.cs
+++ thesis/Controllers/MTVRegistrationstatusController.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using thesis.Data;
+using thesis.Models;
 
 namespace thesis.Controllers
 {
@@ -19,5 +20,42 @@ namespace thesis.Controllers
             var res = await _context.MTVApplications.Include(ve => ve.Vehicle).ToListAsync();
             return View(res);
         }
-    }
+		[HttpPost]
+		public async Task<IActionResult> ProcessApprovement(string action, int vehicleId)
+		{
+			// First, we need to fetch the vehicle record using vehicleId
+			MTVApplication vehicle = await _context.MTVApplications.FindAsync(vehicleId);
+
+			if (vehicle == null)
+			{
+				// Handle the case where the vehicle is not found
+				return NotFound();
+			}
+
+			switch (action)
+			{
+				case "approve":
+					vehicle.Status = "payment";
+					break;
+				case "disapprove":
+					vehicle.Status = "process";
+					break;
+				case "payment":
+					// Logic to check payment details
+					// You might want to redirect to another action here 
+					// or set a specific status to indicate a need for payment.
+					break;
+				default:
+					// Handle unexpected action values
+					return BadRequest();
+			}
+
+			_context.Update(vehicle);
+			await _context.SaveChangesAsync();
+
+			return RedirectToAction("Index", "MTVRegistrationstatus");
+		}
+
+
+	}
 }
